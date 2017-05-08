53eb9de80991ffa677f1eb3a6d4f98680e465b68
YufeiLiu <yufeiliu94@gmail.com>
2017-05-08 20:13:45 +0800

    included Catch as a submodule

--- cmake/catch.cmake
+++
@@ -1,8 +0,0 @@
-function(download_catch)
-  set(URL https://github.com/philsquared/Catch/releases/download/v${CATCH_VERSION}/catch.hpp)
-  set(TARGET ${PROJECT_INCLUDE_DIR}/catch.hpp)
-  message(STATUS "Downloading ${URL}")
-  file(DOWNLOAD ${URL} ${TARGET}
-    EXPECTED_HASH SHA224=${CATCH_SHA224}
-    SHOW_PROGRESS)
-endfunction()
