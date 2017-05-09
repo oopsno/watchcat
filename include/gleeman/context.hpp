2f42ab1d7bd5a35eae8cceed457891eef6eed655
Oopsno <yufeiliu94@gmail.com>
2017-05-09 12:03:38 +0800

    added compile information into Context

--- include/gleeman/context.hpp
+++ include/gleeman/context.hpp
@@ -3,12 +3,36 @@
 
 namespace gleeman {
 
+#ifdef USE_NAT
+#define BOOL_USE_NAT true
+#else
+#define BOOL_USE_NAT false
+#endif
+
+#ifdef USE_CUDA
+#define BOOL_USE_CUDA true
+#else
+#define BOOL_USE_CUDA false
+#endif
+
+#ifdef USE_NVML
+#define BOOL_USE_NVML true
+#else
+#define BOOL_USE_NVML false
+#endif
+
 class Context {
  public:
   void initialize();
   void finalize();
+  static constexpr bool use_nat = BOOL_USE_NAT;
+  static constexpr bool use_cuda = BOOL_USE_CUDA;
+  static constexpr bool use_nvml = BOOL_USE_NVML;
 };
-
 }
+
+#undef BOOL_USE_NAT
+#undef BOOL_USE_CUDA
+#undef BOOL_USE_NVML
 
 #endif //GLEEMAN_CONTEXT_HPP
--- src/gleeman/context.cpp
+++ src/gleeman/context.cpp
@@ -1,23 +1,25 @@
-#include <cuda.h>
-#include <cuda_runtime.h>
-#include <nvml.h>
-
+#include "gleeman/cuda_headers.hpp"
 #include "gleeman/context.hpp"
 #include "gleeman/error.hpp"
 
 namespace gleeman {
 
+constexpr bool Context::use_nat;
+constexpr bool Context::use_cuda;
+constexpr bool Context::use_nvml;
+
 void Context::initialize() {
+#ifdef USE_CUDA
   defaultErrorHandler << cuInit(0);
 #ifdef USE_NVML
   nvmlInit();
-#endif
+#endif //USE_NVML
+#endif //USE_CUDA
 }
 
 void Context::finalize() {
 #ifdef USE_NVML
   nvmlShutdown();
-#endif
+#endif //USE_NVML
 }
-
 }
