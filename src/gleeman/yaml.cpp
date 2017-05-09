2c36dd4d469c81d722f68ea76c505f15f80ef217
Oopsno <yufeiliu94@gmail.com>
2017-05-09 11:56:48 +0800

    fix std::ceil

--- src/gleeman/yaml.cpp
+++ src/gleeman/yaml.cpp
@@ -1,25 +1,26 @@
+#include <cmath>
 #include <iterator>
 
 #include "gleeman/yaml.hpp"
 
 using ull_t = unsigned long long int;
 
-ull_t operator "" _B(ull_t base) { return base; }
-ull_t operator "" _KB(ull_t base) { return base * 1024_B; }
-ull_t operator "" _MB(ull_t base) { return base * 1024_KB; };
-ull_t operator "" _GB(ull_t base) { return base * 1024_MB; };
-ull_t operator "" _sec(ull_t base) { return base; }
-ull_t operator "" _min(ull_t base) { return base * 60_sec; }
-ull_t operator "" _hr(ull_t base) { return base * 60_min; }
-ull_t operator "" _day(ull_t base) { return base * 24_hr; }
-ull_t operator "" _week(ull_t base) { return base * 7_day; }
+ull_t operator"" _B(ull_t base) { return base; }
+ull_t operator"" _KB(ull_t base) { return base * 1024_B; }
+ull_t operator"" _MB(ull_t base) { return base * 1024_KB; };
+ull_t operator"" _GB(ull_t base) { return base * 1024_MB; };
+ull_t operator"" _sec(ull_t base) { return base; }
+ull_t operator"" _min(ull_t base) { return base * 60_sec; }
+ull_t operator"" _hr(ull_t base) { return base * 60_min; }
+ull_t operator"" _day(ull_t base) { return base * 24_hr; }
+ull_t operator"" _week(ull_t base) { return base * 7_day; }
 
 auto parse_first_real(const std::string line) {
   auto begin = line.c_str();
   auto end = begin + line.size();
   char *stop;
   double real = std::strtod(begin, &stop);
-  std::string remains = line.substr(std::distance(const_cast<const char*>(stop), end));
+  std::string remains = line.substr(std::distance((const char *)(stop), end));
   return std::make_tuple(real, remains);
 }
 
